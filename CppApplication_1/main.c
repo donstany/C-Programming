bf47f754fa1e410b62e3b6f9ee8cc3b66b2a229e
Stanislav Stanev <donstany79@gmail.com>
2015-10-28 22:02:24 +0200

    Declare Variables

---
+++ CppApplication_1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: user
+ *
+ * Created on October 28, 2015, 10:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
